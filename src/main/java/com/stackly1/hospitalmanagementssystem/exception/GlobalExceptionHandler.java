74b76b8fd6ff341d1d9d655efccc3160318c0ba2
bhimashi <bhimashigotur6@gmail.com>
2026-05-20 12:57:07 +0530

    added global exception handler

--- src/main/java/com/stackly1/hospitalmanagementssystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stackly1/hospitalmanagementssystem/exception/GlobalExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.stackly1.hospitalmanagementssystem.exception;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex) {
+        return "Doctor ID not found";
+    }
 }
